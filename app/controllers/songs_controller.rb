a4453ec9dd6a7584bc195ac1f6b55e9829d5ad93
Lucy Wall <lhwall@gmail.com>
2018-07-12 14:59:35 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     @song.update(song_params)
     if @song.valid?
-      redirect_to song_path(@song )
+      redirect_to song_path(@song)
     else
       render :edit
     end
