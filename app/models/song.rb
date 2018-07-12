5e09e534b885e8322a10bedce6e03403fb74e4f5
Lucy Wall <lhwall@gmail.com>
2018-07-12 14:11:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,15 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true 
+  validates :title, presence: true
   validates :released, inclusion: { in: %w(true false)}
-  
+  validates :release_year, presence: true :if :released?
+  validates :release_year, :only_integer: true
+  validates :artist_name, presence: true
+  validate :not_in_the_future
+
+  def not_in_the_future
+    if !!self.release_year
+      if self.release_year < Time.new.year
+        errors.add(:release_year, "can't be in the future")
+      end
+    end
 end
