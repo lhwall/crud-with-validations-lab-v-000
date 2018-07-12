8698193efe98be54b86f08eb9f92385c79f35755
Lucy Wall <lhwall@gmail.com>
2018-07-12 14:21:18 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :released, inclusion: { in: %w(true false)}
+  #validates :released, inclusion: { in: %w(true false)}
   validates :release_year, presence: true, if: :released?
   validates :release_year, numericality: {only_integer: true }
   validates :artist_name, presence: true
