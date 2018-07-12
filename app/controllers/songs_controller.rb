4d1992b5d180bfe968f05d9a4654b00c264e1aea
Lucy Wall <lhwall@gmail.com>
2018-07-12 14:53:49 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,6 +29,7 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     if @song.valid?
+      @song.save
       redirect_to song_path(@song)
     else
       render :new
